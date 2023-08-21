5783b252c14187ea4da5f3535cabe8fd094946b8
Rijans Bhagat <82694741+riju951@users.noreply.github.com>
2023-08-21 13:46:04 +0530

    Adding Files
    Adding Files

---
+++ Srcipt.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cf77e1c05emsh2f541702bb13409p1d873bjsna310e80f7e07',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
